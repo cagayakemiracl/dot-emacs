e881698a8c5b96c8846831e25aea29eaf3b89f74
cagayakemiracl <hirosnonno1182@gmail.com>
2015-01-24 23:57:16 +0900

    enh-ruby-modeにするのを忘れていた

--- boot/51-ruby.el
+++ boot/51-ruby.el
@@ -28,7 +28,7 @@
 (autoload 'enh-ruby-mode "enh-ruby-mode"
   "Mode for editing ruby source files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$latex " . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
 (setq interpreter-mode-alist (append '(("ruby" . enh-ruby-mode))
