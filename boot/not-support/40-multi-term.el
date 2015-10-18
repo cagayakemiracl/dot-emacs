598f387e1cb13a39efa3f1a8a9f1e1fef9b27e1c
cagayakemiracl <hirosnonno1182@gmail.com>
2015-10-18 16:57:19 +0900

    not support

--- boot/not-support/40-multi-term.el
+++ boot/not-support/40-multi-term.el
@@ -27,16 +27,17 @@
 ;;term
 (require 'multi-term)
 (setq multi-term-program "/bin/zsh")
-(add-hook 'term-mode-hook '(lambda ()
-			     (define-key term-raw-map "\C-y" 'term-paste)
-			     (define-key term-raw-map "\C-q" 'move-beginning-of-line)
-			     (define-key term-raw-map "\C-f" 'forward-char)
-			     (define-key term-raw-map "\C-b" 'backward-char)			     
-			     (define-key term-raw-map "\C-t" 'set-mark-command)			     
-			     (define-key term-raw-map (kbd "ESC") 'term-send-raw)
-			     (define-key term-raw-map [delete] 'term-send-raw)
-                             (define-key term-raw-map "\C-z"
-                               (lookup-key (current-global-map) "\C-z"))))
+(add-hook 'term-mode-hook
+					'(lambda ()
+						 (define-key term-raw-map "\C-y" 'term-paste)
+						 (define-key term-raw-map "\C-q" 'move-beginning-of-line)
+						 (define-key term-raw-map "\C-f" 'forward-char)
+						 (define-key term-raw-map "\C-b" 'backward-char)			     
+						 (define-key term-raw-map "\C-t" 'set-mark-command)			     
+						 (define-key term-raw-map (kbd "ESC") 'term-send-raw)
+						 (define-key term-raw-map [delete] 'term-send-raw)
+						 (define-key term-raw-map "\C-z"
+							 (lookup-key (current-global-map) "\C-z"))))
 (global-set-key (kbd "C-c n") 'multi-term-next)
 (global-set-key (kbd "C-c p") 'multi-term-prev)
 
